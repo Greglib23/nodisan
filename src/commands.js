f4c52068c79e204299589689c10b0a178bfbb18c
Greglib23 <cristianisaias@outlook.com.ar>
2024-04-07 08:24:39 +0100

    Added timing to the commands

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,8 @@
 import { exec } from 'child_process';
 import * as fs from 'fs';
 
+let perf
+
 const addingPropsTs = () => {
     const tsconfigContent = fs.readFileSync('tsconfig.json', 'utf8');
     const tsconfigContentWithoutComments = tsconfigContent.replace(/\/\/.*/g, '').replace(/\/\*[\s\S]*?\*\//g, '');
@@ -27,18 +29,23 @@ const installCommands = async () => {
                     console.error(`Error running dependencies: ${error}`);
                     return;
                 }
+                let time = Math.floor((performance.now() - perf) * 100) / 100
+                console.log(`\u001B[1m\u001B[32mDONE in ${time}ms\u001B[39m\u001B[22m`)
                 resolve()
             });
         })
 
     }
     console.log("Installing dev dependencies...")
+    perf = performance.now()
     await runCommand(comm1)
 
     console.log("Installing dependencies...")
+    perf = performance.now()
     await runCommand(comm2)
 
     console.log("Running tsc...")
+    perf = performance.now()
     await runCommand(comm3)
 
     addingPropsTs()
