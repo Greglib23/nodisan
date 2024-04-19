1ad047dd9c968fd839ec0ad7bf3c6b9f970a0156
Greglib23 <cristianisaias@outlook.com.ar>
2024-04-19 04:07:13 +0100

    Added command "node nodisan migrate:rollback", "node nodisan migrate:rollback --step=x". Adopted a new form to handle nodisan migrations, via .json files

--- src/handleArguments.js
+++ src/handleArguments.js
@@ -2,6 +2,7 @@ import { startProject } from "../index.js"
 import { runCommand, runVite } from "./commands.js"
 import { copyFile, verificatePath, makeFlag, readOriginFile, copyFiles, writeDestinyFile, createDir } from "./fileGenerator.js"
 import * as fs from 'fs'
+import { join as pathJoin } from 'path';
 
 
 const templatePaths = "./node_modules/nodisan/templates/"
@@ -59,7 +60,8 @@ export const handleArguments = async (args) => {
 const handleMigrate = async (args) => {
     if (args.length == 3) {
         if (args[2] == "migrate") {
-            await runCommand("npx prisma migrate dev",
+            await migrateToPrisma()
+            await runCommand("npx prisma migrate dev --name '-'",
                 "Running migrations...",
                 "Migrations sended!")
             return
@@ -73,6 +75,20 @@ const handleMigrate = async (args) => {
         if (args[2] == "migrate:status") {
             await runCommand("npx prisma migrate status",
                 "Running status...")
+            return
+        }
+        if (args[2] == "migrate:rollback") {
+            await removeMigration(1)
+            await runCommand("npx prisma migrate reset --force",
+                "Running rollback...")
+            return
+        }
+    }
+    if (args.length == 4) {
+        if (args[2] == "migrate:rollback" && args[3].split("=")[0] == "--step") {
+            await removeMigration(args[3].split("=")[1])
+            await runCommand("npx prisma migrate reset --force",
+                "Running rollback...")
             return
         }
     }
@@ -261,14 +277,15 @@ const handleBuild = async (args) => {
     }
 }
 const makeMigration = async (migName) => {
+    let originFile = await readOriginFile("./node_modules/nodisan/templates/voids/create_table_name.json")
     let fileName = await getMigrationName(migName)
     let migPath = "./src/database/migrations"
     let isCreated = await verificatePath(migPath)
     if (isCreated) {
-        await writeDestinyFile(migPath + "/" + fileName, "")
+        await writeDestinyFile(migPath + "/" + fileName, originFile)
     } else {
         await createDir(migPath)
-        await writeDestinyFile(migPath + "/" + fileName, "")
+        await writeDestinyFile(migPath + "/" + fileName, originFile)
     }
 }
 const getMigrationName = async (migName) => {
@@ -285,10 +302,10 @@ const getMigrationName = async (migName) => {
     const completeDate = `${year}_${month.toString().padStart(2, '0')}_${day.toString().padStart(2, '0')}_`;
     const completeHour = `${hours.toString().padStart(2, '0')}${minutes.toString().padStart(2, '0')}${secconds.toString().padStart(2, '0')}`;
     if (migName) {
-        let fileName = completeDate + completeHour + "_" + migName + ".js"
+        let fileName = completeDate + completeHour + "_" + migName + ".json"
         return fileName
     } else {
-        let fileName = completeDate + completeHour + ".js"
+        let fileName = completeDate + completeHour + ".json"
         return fileName
     }
 }
@@ -301,10 +318,66 @@ const makeModel = async (modelName, migration = false) => {
         console.log(prompt + `File "${modelName}.ts" alredy exists.`)
         return
     } else {
-        await writeDestinyFile(modelPath + "/" + modelName + ".js", dataModel)
-        await replaceInFile(modelPath + "/" + modelName + ".js", "{ modelName }", modelName)
-        await writeDestinyFile(modelPath + "/" + modelName + ".interface.js", dataInterface)
-        await replaceInFile(modelPath + "/" + modelName + ".interface.js", "{ modelName }", modelName.charAt(0).toUpperCase() + modelName.slice(1))
+        await writeDestinyFile(modelPath + "/" + modelName + ".ts", dataModel)
+        await replaceInFile(modelPath + "/" + modelName + ".ts", "{ modelName }", modelName)
+        await writeDestinyFile(modelPath + "/" + modelName + ".interface.ts", dataInterface)
+        await replaceInFile(modelPath + "/" + modelName + ".interface.ts", "{ modelName }", modelName.charAt(0).toUpperCase() + modelName.slice(1))
     }
     if (migration) await makeMigration("create_" + modelName + "s_table")
+}
+const migrateToPrisma = async () => {
+    const migrationsDir = './src/database/migrations/'
+    const schemaFile = './prisma/schema.prisma'
+    await copyFile(templatePaths + "schema.prisma", "./prisma/schema.prisma", true)
+    fs.readdirSync(migrationsDir).forEach(file => {
+        if (file.endsWith('.json')) {
+            let tableName = file.replace(/\.json$/, '').replace(/_/g, ' ').split(" ")
+            if (tableName.length > 4) {
+                tableName = tableName[tableName.length - 2].slice(0, -1)
+            } else {
+                tableName = "default"
+            }
+            let toExport = `model ${tableName} {\n`
+            const migrationPath = pathJoin(migrationsDir, file);
+            const migrationContent = fs.readFileSync(migrationPath, 'utf8');
+            let data = migrationContent
+            data = JSON.parse(data.toString())
+
+            for (const key in data) {
+                if (key == "id") {
+                    toExport += "  id Int @id @default(autoincrement())\n"
+                }
+                if (key == "timestamps") {
+                    toExport += "  created_at DateTime @default(now())\n"
+                    toExport += "  modified_at DateTime @updatedAt\n"
+                }
+                if (key !== "id" && key !== "timestamps") {
+                    let props = ""
+                    for (const e in data[key]) {
+                        if (data[key][e] == 'string') {
+                            props += "String "
+                        }
+                        if (data[key][e] == 'unique') {
+                            props += "@unique "
+                        }
+                    }
+                    toExport += `  ${key} ${props}\n`
+                }
+            }
+            toExport += "}\n"
+            toExport += "\n"
+            fs.appendFileSync(schemaFile, toExport)
+        }
+    })
+}
+const removeMigration = async (cant) => {
+    const migrationsDir = './prisma/migrations';
+    const migrations = fs.readdirSync(migrationsDir)
+        .filter(file => fs.statSync(pathJoin(migrationsDir, file)).isDirectory())
+        .sort((a, b) => fs.statSync(pathJoin(migrationsDir, b)).ctime.getTime() - fs.statSync(pathJoin(migrationsDir, a)).ctime.getTime());
+    migrations.slice(0, cant).forEach(migracion => {
+        const migracionPath = pathJoin(migrationsDir, migracion);
+        fs.rmSync(migracionPath, { recursive: true, force: true });
+        console.log(`Migración eliminada: ${migracion}`);
+    });
 }
--- templates/2024_04_16_085521_create_users_table.js
+++
@@ -1,7 +0,0 @@
-const user = {
-    id,
-    email: ["string", "unique"],
-    password: ["string"],
-    timestamps
-}
-export default user
